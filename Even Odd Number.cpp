d912956e4742c8d97e0dbdaafc58f7c3730f1a5a
YUGANK742 <78154174+YUGANK742@users.noreply.github.com>
2021-01-29 08:59:33 +0530

    Add files via upload

---
+++ Even Odd Number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	system("color 1e");
+	int a;
+	cout<<"Enter A Number  ";
+	cin>> a;
+	if (a%2==0)
+	{
+		cout<<"\n It is an Even Number  ";
+		}
+		else
+		{
+			cout<<"\n It is an Odd Number";
+			}	
+			return 0;
+}
