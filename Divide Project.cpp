ebc579e08dd9ad14b6e04a78bfff2b7a3cb1a184
YUGANK742 <78154174+YUGANK742@users.noreply.github.com>
2021-01-30 09:02:38 +0530

    Add files via upload

---
+++ Divide Project.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	system ("color 04");
+	long long int y,z,a;
+	cout<< "\n Enter First Number : ";
+	cin>> y;
+	cout<<"\n Enter Second Number : ";
+	cin>> z;
+	a=y/z;
+	cout<<"\n The Answer is: "<<a;
+	return	 0;
+	
+}
